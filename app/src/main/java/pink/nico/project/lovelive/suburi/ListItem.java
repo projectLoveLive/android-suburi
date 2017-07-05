3f64df60714f0e60b288feb2a5d9eb1104dbb975
UMI-NOTE\UMI <f91seed@yahoo.co.jp>
2017-07-06 01:23:59 +0900

    最低限の画面遷移とリスト表示 不要コードの削除、紹介画面の追加

---
+++ app/src/main/java/pink/nico/project/lovelive/suburi/ListItem.java
@@ -0,0 +1,15 @@
+package pink.nico.project.lovelive.suburi;
+
+import lombok.Data;
+
+/**
+ * Created by UMI on 2017/07/07.
+ */
+
+@Data
+public class ListItem {
+    private long id = 0;
+    private String characterName = null;
+    private String desc = null;
+
+}
